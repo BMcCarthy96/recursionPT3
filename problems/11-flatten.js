65c7d2379e57a6de63b1ab63788155bc8c6953a0
Brandon McCarthy <Bmac96.dev@gmail.com>
2024-04-11 21:03:27 -0400

    Added new solution for flatten

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -13,18 +13,34 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
 function flatten(arr) {
-  debugger
-const result = [];
-arr.forEach((item) => {
-  if (Array.isArray(item)) {
-    result.push(...flatten(item));
-  } else {
-    result.push(item);
-  }
-})
- return result
+    debugger;
+    const result = [];
+    arr.forEach((item) => {
+        if (Array.isArray(item)) {
+            result.push(...flatten(item));
+        } else {
+            result.push(item);
+        }
+    });
+    return result;
 }
-  
+
+// function flatten(arr) {
+//     let flattened = [];
+
+//     if (arr.length === 0) {
+//         return flattened;
+//     }
+
+//     for (let element of arr) {
+//         if (Array.isArray(element)) {
+//             flattened = flattened.concat(flatten(element));
+//         } else {
+//             flattened.push(element);
+//         }
+//     }
+//     return flattened;
+// }
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
